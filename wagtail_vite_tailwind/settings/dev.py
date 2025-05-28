61af966b919cf6c671974ce3a02dc39ceeb0ee0d
Steven Steinwand <steven_ts@hotmail.com>
2025-05-28 10:22:13 -0600

    First commit

---
+++ wagtail_vite_tailwind/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-req%_6a$i77xqk*&g&)alhc_ipeh2td@)j$=drj4!3xm=6moh4"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
